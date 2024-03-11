5348c43a09b90b8210041d636c59b9cf5c55dad8
mgDentist <gmi1987@gmail.com>
2024-03-11 14:01:48 +0400

    module2-theme2

---
+++ js/modal.js
@@ -0,0 +1,6 @@
+'use strict';
+
+const modalTitle = document.querySelector('.modal__title');
+const modalForm = document.querySelector('.modal__form');
+const modalCheckbox = document.querySelector('input.modal__checkbox[type="checkbox"]');
+const modalCheckboxField = document.querySelector('modal__input_discount');
