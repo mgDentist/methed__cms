ce4452e4d830c211bc21f84fbb0f22332da89526
mgDentist <gmi1987@gmail.com>
2024-03-12 00:11:49 +0400

    Правит опечатку

--- js/modal.js
+++ js/modal.js
@@ -3,4 +3,4 @@
 const modalTitle = document.querySelector('.modal__title');
 const modalForm = document.querySelector('.modal__form');
 const modalCheckbox = document.querySelector('input.modal__checkbox[type="checkbox"]');
-const modalCheckboxField = document.querySelector('modal__input_discount');
+const modalCheckboxField = document.querySelector('.modal__input_discount');
