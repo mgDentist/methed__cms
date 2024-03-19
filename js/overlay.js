1e2c76c68b193972cd279e922785bbca37287542
mgDentist <gmi1987@gmail.com>
2024-03-19 16:44:43 +0400

    module02-theme05-task-cms

--- js/overlay.js
+++ js/overlay.js
@@ -1,5 +1,29 @@
 'use strict';
 
-const overlay = document.querySelector('.overlay');
+const showOverlay = () => {
+  const overlay = document.querySelector('.overlay');
+  const addItemButton = document.querySelector('.panel__add-goods');
+  const closeModalButton = document.querySelector('.modal__close');
+  const modal = document.querySelector('.modal');
 
-overlay.classList.remove('active');
+  overlay.classList.remove('active');
+
+  addItemButton.addEventListener('click', () => {
+    overlay.classList.add('active');
+  });
+
+  closeModalButton.addEventListener('click', () => {
+    overlay.classList.remove('active');
+  });
+
+  overlay.addEventListener('click', () => {
+    if (overlay.classList.contains('active')) {
+      overlay.classList.remove('active');
+      modal.addEventListener('click', (e) => {
+        e.stopPropagation();
+      });
+    }
+  });
+};
+
+showOverlay();
